f38316c143f624e333ddda8f27ee015ee7cc11d1
Huggychan <huggydegozaru@gmail.com>
2016-05-08 19:12:22 -0700

    edits to caesar cipher

--- Ciphers and Encryptions/CryptoGui/src/Main.java
+++ Ciphers and Encryptions/CryptoGui/src/Main.java
@@ -1,17 +1,35 @@
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.NumberAxis;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("caesarCipher.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("caesarCipher2.fxml"));
         primaryStage.setTitle("If You're Not First, You're Last");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root, 300, 275);
+        primaryStage.setScene(scene);
         primaryStage.show();
+
+        /*scene.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                primaryStage.minHeightProperty().bind(scene.heightProperty());
+            }
+        });
+
+        scene.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                primaryStage.minWidthProperty().bind(scene.widthProperty());
+            }
+        });*/
     }
 
 
